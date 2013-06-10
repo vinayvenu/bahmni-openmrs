5cb866d176fdfe53172d5bb20b70c86ce1f13f9d
Pulkit <pulkit.bosco05@gmail.com>
2013-06-10 15:34:09 +0530

    Mario, pulkit | Creating one repo which packages together all the client apps.

---
+++ openmrs-module-bahmni.opd/README.md
@@ -0,0 +1,7 @@
+
+This repository simply bundles the OPD client angular app by
+cloning it into the webapp folder of the omod, thereby creating an
+OpenMRS compatible omod structure. It also defines registration as an
+app, and can thus be installed by the OpenMRS module interface and also
+participate in the app icon drawing.
+
---
+++ openmrs-module-bahmni.opd/omod/src/main/resources/fetch_repo.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+echo "Fetching repo $1"
+
+baseDir=$(dirname $0)
+outputDir=$baseDir/../webapp/resources/
+
+repoName=`echo $1 | sed 's/^.*\/\(.*\)\.git/\1/'`
+
+if [ ! -d "$outputDir/$repoName" ]; then
+  echo "Repo doesn't exist. Cloning into $outputDir"
+  mkdir -p $outputDir
+  cd $outputDir
+  git clone $1
+  cd -
+else
+  echo "Repo exists $outputDir/$repoName. Updating."
+  cd $outputDir/$repoName
+  git pull --rebase
+  cd -
+fi
