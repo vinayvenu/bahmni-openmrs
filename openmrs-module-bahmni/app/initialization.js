9ae7cb903a9cf5a7dbffe6bfb64e4f19cc139c96
Mário Areias <mario.s.s.areias@gmail.com>
2013-06-12 11:43:11 +0530

    moving the app stuff for other repository

--- openmrs-module-bahmni/app/initialization.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('bahmni-app.initialization', [])
-    .factory('initialization', ['$rootScope', '$q', function($rootScope, $q){
-        var initializationPromiseDefer = $q.defer();
-        
-        return initializationPromiseDefer.promise;
-    }]);
--- openmrs-module-bahmni/app/modules/home/controllers/homePageController.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('openmrs-module-bahmni.homepage',['resources.appService'])
-	.controller('HomePageController', ['$scope', 'appService', function($scope, appService){
-		$scope.apps = appService.enabledAppsOnHomePage();
-
-	}]);
